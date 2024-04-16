e7b1df0d0a78b03beb8db9175afd2847d49e903e
Emiliya <emiliq.cenkovska@gmail.com>
2024-04-16 16:09:38 +0300

    add tests project and packages - beginning

---
+++ Mezeta.UnitTests/MezetaServiceTests.cs
@@ -0,0 +1,16 @@
+namespace Mezeta.UnitTests
+{
+    public class MezetaServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
